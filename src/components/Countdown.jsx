f1926013f982b635c57095fd92ed133d3bff32b0
JohnnatanV <joh.villaneda@outlook.com>
2024-12-24 18:24:05 -0500

    Correction of the countdown timer

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const COUNTDOWN_T = new Date("2024-12-26T11:59:59");
+const COUNTDOWN_T = new Date("2024-12-26T17:59:59");
 // const TEST_TARGET = new Date("2024-12-21T23:02:01");
 
 const getTimeLeft = () => {
