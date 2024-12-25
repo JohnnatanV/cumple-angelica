cd41daaf3e73eaee0f1d172c55adf14e1dacba94
JohnnatanV <joh.villaneda@outlook.com>
2024-12-24 23:17:59 -0500

    Defined adjustments to media querys for cellphone layout

--- src/components/Present.jsx
+++ src/components/Present.jsx
@@ -2,16 +2,17 @@ import { motion } from "motion/react";
 import Collage from "./Collage";
 
 const Present = () => {
-  const message = "Feliz cumpleaños, Muñequita hermosa";
+  const message =
+    "Un mensaje de todos los que te amamos, \n los que estamos a tu lado, \n los que estan lejos \n y los que ya no pueden estar. \n Feliz cumpleaños, Muñequita hermosa";
   return (
     <div className="present">
       <motion.h1
         className="kaushan-script-regular message"
         initial={{ opacity: 0, scale: 0, color: "#ffffff" }}
-        animate={{ opacity: 1, scale: 1.5, color: "#222" }}
+        animate={{ opacity: 1, scale: 1.15, color: "#222" }}
         transition={{
           duration: 1.5,
-          delay: 5,
+          delay: 8,
           type: "spring",
           visualDuration: 1,
           bounce: 0.5,
